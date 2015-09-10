ffb6a4bce9911a79ca055a9fc34f41d871d9b206
Jean-Philippe Evrard <jean-philippe@evrard.me>
2015-09-10 12:35:43 +0200

    Improved HAproxy example scripts
    Additional cosmetic changes also included

--- files/keepalived_notifications.sh
+++ files/keepalived_notifications.sh
@@ -15,46 +15,21 @@
 
 TYPE=$1
 NAME=$2
+NOW=`date "+%Y-%m-%d %H:%M:%S"`
 NEWSTATE=$3
 OLDSTATE=$(cat /var/run/keepalived.state)
 
-echo "Changed state from (probably) $OLDSTATE to $NEWSTATE" >> /var/log/keepalived-states.log
-
 echo "$NEWSTATE" > /var/run/keepalived.state
 
-if [ "$OLDSTATE" -ne "FAULT" ]
 case $NEWSTATE in
-        "MASTER") service haproxy start
-                  exit 0
-                  ;;
-        "BACKUP") service haproxy stop
-                  exit 0
-                  ;;
-        "FAULT") service haproxy stop
+        "FAULT") echo "$NOW Trying to restart haproxy to get out"\
+                  "of faulty state" >> /var/log/keepalived-notifications.log
+                 /etc/init.d/haproxy stop
+                 /etc/init.d/haproxy start
                  exit 0
                  ;;
-        *) echo "unknown state"
+        *) echo "$NOW Unknown state" >> /var/log/keepalived-notifications.log
            exit 1
            ;;
 
 esac
-else
-#If something was faulty, try to get standard behaviour first.
-echo "This could happen if your keepalived process ungracefully fails" >> /var/log/keepalived-states.log
-case $NEWSTATE in
-        "MASTER") service haproxy start
-           service keepalived restart
-           exit 0
-           ;;
-        "BACKUP") service haproxy start
-           service keepalived restart
-           exit 0
-           ;;
-        "FAULT") service haproxy start
-                 exit 0
-                 ;;
-        *) echo "unknown state"
-           exit 1
-           ;;
-esac
-fi
